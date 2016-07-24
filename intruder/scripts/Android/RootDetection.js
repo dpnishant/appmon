5a7d471e3f46d902c9db0f577c993d04119d52ee
Nishant Das Patnaik <nishant.dp@gmail.com>
2016-07-25 01:09:51 +0530

    bug fix

--- intruder/scripts/Android/RootDetection.js
+++ intruder/scripts/Android/RootDetection.js
@@ -12,28 +12,24 @@ var rooted_blacklist = [
 ];
 Java.perform(function () {
 	var File = Java.use("java.io.File");
+	var Runtime = Java.use("java.lang.Runtime");
 	File.exists.implementation = function () {
 		if (rooted_blacklist.indexOf(this.path['value']) > -1) {
-			console.log(this.path['value']);
+			console.log("Dropped File Access: " + this.path['value']);
 			return false;
 		} else {
 			return this.exists.call(this);
 		}
 		
 	}
-});
-
-Java.perform(function (){
-	var Runtime = Java.use("java.lang.Runtime");
-	console.log(typeof Runtime.getRuntime());
+	
 	Runtime.getRuntime().exec.overload("java.lang.String").implementation = function(command) {
-		console.log('exec: ' + command.toString());
 		if(command.toString() === 'su') {
+			console.log("Dropped Command: su");
 			return null;
 		} else {
 			return this.exec.call(this, command);	
 		}
-		
 	};
 
 });
