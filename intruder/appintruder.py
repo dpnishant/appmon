55c61aa8933250f8accab50ea575aceeacb6a63a
Nishant Das Patnaik <nishant.dp@gmail.com>
2016-08-04 05:06:44 -0700

    add spawn feature to intruder

--- intruder/appintruder.py
+++ intruder/appintruder.py
@@ -8,6 +8,14 @@ merged_script_path = '/tmp/merged.js'
 def on_detached():
     print colored('[WARNING] "%s" has terminated!' % (app_name), 'red')
 
+def _exit_():
+    print colored('[INFO] Exiting...', 'green')
+    try:
+        os.remove(merged_script_path)
+    except Exception as e:
+        pass
+    sys.exit(0)
+
 def on_message(message, data):
     current_time = time.strftime("%H:%M:%S", time.localtime())
     if message['type'] == 'send':
@@ -55,6 +63,8 @@ def init_opts():
                     "com.twitter.android" for Android; "Twitter" for MacOS X''')
     parser.add_argument('-p', action='store', dest='platform', default='',
                     help='Platform Type; Accepts "ios", "android" or "mac"')
+    parser.add_argument('--spawn', action='store', dest='spawn', default=0,
+                    help='''Optional; Accepts 1=Spawn, 0=Attach; Needs "-p PLATFORM"''')
     parser.add_argument('-ls', action='store', dest='list_apps', default=0,
                     help='''Optional; Accepts 1 or 0; Lists running Apps on target device; Needs "-p PLATFORM"''')
     parser.add_argument('-s', action='store', dest='script_path', default='',
@@ -69,6 +79,7 @@ def init_opts():
     platform = results.platform
     script_path = results.script_path
     list_apps = results.list_apps
+    spawn = int(results.spawn)
 
     if script_path != None and app_name == '' and list_apps == 0:
         parser.print_help()
@@ -78,21 +89,109 @@ def init_opts():
         parser.print_help()
         sys.exit(1)
 
-    return app_name, platform, script_path, list_apps
+    return app_name, platform, script_path, list_apps, spawn
+
+def getDisplayName(session, app_name):
+    try:
+        script = session.create_script("""/* ____CFBundleDisplayName Getter for Gadget____ */
+'use strict';
+rpc.exports = {
+  gadgetdisplayname: function () {
+    if (ObjC.available) {
+      var dict = ObjC.classes.NSBundle.mainBundle().infoDictionary();
+      var iter = dict.keyEnumerator();
+      var key = "";
+      while ((key = iter.nextObject()) !== null) {
+        if (key.toString() === "CFBundleDisplayName") {
+          return dict.objectForKey_(key).toString();
+        }
+      }
+    } else { return null; }
+  }
+};
+""")
+        script.load()
+        if script.exports.gadgetdisplayname():
+            app_name = script.exports.gadgetdisplayname()
+        script.unload()
+        return app_name
+    except Exception as e:
+        print colored("[ERROR] " + str(e), "red")
+        traceback.print_exc()
+
+
+def getBundleID(device, app_name, platform):
+    try:
+        session = device.attach(app_name)
+        session.on('detached', on_detached)
+        script = session.create_script("""'use strict';
+rpc.exports = {
+  iosbundleid: function () {
+    return ObjC.classes.NSBundle.mainBundle().bundleIdentifier().toString();
+  },
+  macosbundleid: function () {
+    return ObjC.classes.NSBundle.mainBundle().executablePath().toString();
+  }
+};
+""")
+        script.load()
+        if platform == 'ios':
+            bundleID = script.exports.iosbundleid()
+        elif platform == 'macos':
+            bundleID = script.exports.macosbundleid()
+        script.unload()
+        session.detach()
+        return bundleID
+    except Exception as e:
+        print colored("[ERROR] " + str(e), "red")
+        traceback.print_exc()
 
 def init_session():
     try:
         session = None
         if platform == 'ios' or platform == 'android':
-            device = frida.get_usb_device()
-        elif platform == 'mac':
+            try:
+                device = frida.get_usb_device()
+            except Exception as e:
+                print colored(str(e), "red")
+                traceback.print_exc()
+                if platform == 'android':
+                    print colored("Troubleshooting Help", "blue")
+                    print colored("HINT: Is USB Debugging enabled?", "blue")
+                    print colored("HINT: Is `frida-server` running on mobile device (with +x permissions)?", "blue")
+                    print colored("HINT: Is `adb` daemon running?", "blue")
+                    sys.exit(1)
+                elif platform == "ios":
+                    print colored("Troubleshooting Help", "blue")
+                    print colored("HINT: Have you installed `frida` module from Cydia?", "blue")
+                    print colored("HINT: Have used `ipa_installer` to inject the `FridaGadget` shared lbrary?", "blue")
+                    sys.exit(1)
+        elif platform == 'macos':
             device = frida.get_local_device()
         else:
-            print colored('[ERROR] Unsupported platform', 'red')
-            sys.exit()
+            print colored('[ERROR] Unsupported Platform', 'red')
+            sys.exit(1)
+        pid = None
         if app_name:
             try:
-                session = device.attach(app_name)
+                if platform == 'android' and spawn == 1:
+                    print colored("Now Spawning %s" % app_name, "green")
+                    pid = device.spawn([app_name])
+                    session = device.attach(pid)
+                elif (platform == 'ios' or platform == 'macos') and spawn == 1:
+                    bundleID = getBundleID(device, app_name, platform)
+                    print pid
+                    if bundleID:
+                        print colored("Now Spawning %s" % bundleID, "green")
+                        pid = device.spawn([bundleID])
+                        print pid
+                        session = device.attach(pid)
+                    else:
+                        print colored("[ERROR] Can't spawn %s" % app_name, "red")
+                        traceback.print_exc()
+                        sys.exit(1)
+                else:
+                    session = device.attach(app_name)
             except Exception as e:
                 print colored('[ERROR] ' + str(e), 'red')
                 traceback.print_exc()
@@ -103,34 +202,35 @@ def init_session():
         print colored('[ERROR] ' + str(e), 'red')
         traceback.print_exc()
         sys.exit(1)
-    return device, session
+    return device, session, pid
 
-if __name__ == '__main__':
-    try:
-        app_name, platform, script_path, list_apps = init_opts()
-        
-        if script_path != None and app_name == None:
-            parser.print_help()
-            sys.exit(1)
+try:
+    app_name, platform, script_path, list_apps, spawn = init_opts()
+    device, session, pid = init_session()
 
-        device, session = init_session()
+    if int(list_apps) == 1:
+        list_processes(device)
+        sys.exit(0)
 
-        if int(list_apps) == 1:
-            list_processes(device)
-            sys.exit(1)
+    if session:
+        if app_name == "Gadget":
+            app_name = getDisplayName(session, app_name)
+        script = session.create_script(generate_injection())
+        if script:
+            print colored('[INFO] Instrumentation started...', 'yellow')
+            script.on('message', on_message)
+            script.load()
+            if spawn == 1 and pid:
+                device.resume(pid)
+except Exception as e:
+    print colored('[ERROR] ' + str(e), 'red')
+    traceback.print_exc()
+    sys.exit(1)
 
-        if session:
-            script = session.create_script(generate_injection())
-            if script:
-                print colored('[INFO] Intrusion started...', 'yellow')
-                script.on('message', on_message)
-                script.load()
-    except Exception as e:
-        print colored('[ERROR] ' + str(e), 'red')
-        traceback.print_exc()
-        sys.exit(1)
-    try:
-        while True:
-            pass
-    except KeyboardInterrupt:
-        sys.exit(1)
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    script.unload()
+    session.detach()
+    _exit_()
