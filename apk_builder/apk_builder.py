d03e2eb2437dc72c9642894e61445d14272420ba
Androz <androz2091@gmail.com>
2021-02-11 13:49:23 +0100

    fix inconsistent tabs and spaces

--- apk_builder/apk_builder.py
+++ apk_builder/apk_builder.py
@@ -96,10 +96,10 @@ try:
 	package_name = apk_dump.split("package: name=")[1].split(" ")[0].strip("'\"\n\t ")
 	manifest_file_path = os.path.join(WORK_DIR, package_name, "AndroidManifest.xml")
         try:
-            launchable_activity = apk_dump.split("launchable-activity: name=")[1].split(" ")[0].strip("'\"\n\t ")
+		launchable_activity = apk_dump.split("launchable-activity: name=")[1].split(" ")[0].strip("'\"\n\t ")
         except IndexError:
-            print("No launchable activity found")
-            sys.exit(1)
+            	print("No launchable activity found")
+            	sys.exit(1)
         launchable_activity_path = os.path.join(WORK_DIR, package_name, "smali", launchable_activity.replace(".", "/") + ".smali")
 
 	new_apk_path = WORK_DIR + "/" + package_name + ".apk"
